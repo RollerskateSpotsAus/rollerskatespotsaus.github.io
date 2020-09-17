11e8d774d8fa295c1491268f828c05fbe207ca52
Celine <celinelindeque@gmail.com>
2020-09-17 16:20:07 +1000

    each word instead of letter, looks clunky

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Wrap every letter in a span
 var textWrapper = document.querySelector('.ml7 .letters');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+textWrapper.innerHTML = textWrapper.textContent.replace(/\w+/g, "<span class='word'>$&</span>");
 
 anime.timeline({loop: false})
   .add({
-    targets: '.ml7 .letter',
+    targets: '.ml7 .word',
     translateY: ["1.1em", 0],
     translateX: ["0.55em", 0],
     translateZ: 0,
